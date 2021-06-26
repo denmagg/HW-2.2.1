38d66ccf9d154ab7921220d77b1529d42a698ffe
Denis <denmagg.work@gmail.com>
2021-06-26 09:38:35 +0300

    Changed IBOutlet on IBOutletCollection Added editTF method

--- HW 2.2.1/ViewController.swift
+++ HW 2.2.1/ViewController.swift
@@ -8,55 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var rgbView: UIView!
     
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
-    
-    @IBOutlet var redSliderOutlet: UISlider!
-    @IBOutlet var greenSliderOutlet: UISlider!
-    @IBOutlet var blueSliderOutlet: UISlider!
-    
-    @IBOutlet var redTF: UITextField!
-    @IBOutlet var greenTF: UITextField!
-    @IBOutlet var blueTF: UITextField!
+    @IBOutlet var rgbLabels: [UILabel]!
+    @IBOutlet var rgbTFs: [UITextField]!
+    @IBOutlet var rgbSliders: [UISlider]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for rgbTF in rgbTFs {
+            rgbTF.delegate = self
+        }
+        rgbSlide()
+    }
+    
+    //скрытие клавиатуры по тапу по экрану
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //функция кнопки return на клавиатуре
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //скрывает клавиатуру
+        textField.resignFirstResponder()
+        return true
     }
 
-    
-    @IBAction func redSlider() {
-        redLabel.text = String(redSliderOutlet.value)
-        redTF.text = String(redSliderOutlet.value)
-        rgbView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
-                                          green: CGFloat(greenSliderOutlet.value),
-                                          blue: CGFloat(blueSliderOutlet.value),
-                                          alpha: 1.0)
-    }
-    @IBAction func greenSlider() {
-        greenLabel.text = String(greenSliderOutlet.value)
-        greenTF.text = String(greenSliderOutlet.value)
-        rgbView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
-                                          green: CGFloat(greenSliderOutlet.value),
-                                          blue: CGFloat(blueSliderOutlet.value),
+    //общий IBAction для редактрирования цвета
+    @IBAction func rgbSlide() {
+        for i in stride(from: 0, to: 3, by: 1){
+            rgbLabels[i].text = String(round(rgbSliders[i].value * 100) / 100)
+            rgbTFs[i].text = String(round(rgbSliders[i].value * 100) / 100)
+        }
+        rgbView.backgroundColor = UIColor(red: CGFloat(rgbSliders[0].value),
+                                          green: CGFloat(rgbSliders[1].value),
+                                          blue: CGFloat(rgbSliders[2].value),
                                           alpha: 1.0)
     }
     
-    @IBAction func blueSlider() {
-        blueLabel.text = String(blueSliderOutlet.value)
-        blueTF.text = String(blueSliderOutlet.value)
-        rgbView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value),
-                                          green: CGFloat(greenSliderOutlet.value),
-                                          blue: CGFloat(blueSliderOutlet.value),
-                                          alpha: 1.0)
+//    Действие при редактрировании текстрового поля
+    @IBAction func editTF() {
+        for (rgbTF, rgbSlider) in zip(rgbTFs, rgbSliders) {
+//          если удалось извлечь цифру
+            if let colorValue: Float = Float(rgbTF.text!) {
+                rgbSlider.value = colorValue
+                rgbSlide()
+            }
+            else
+            {
+                let alert = UIAlertController(title: "Error", message: "Please, enter a number in range 0..1", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                present(alert, animated: true)
+            }
+        }
     }
-    
-    
-    
 }
 
