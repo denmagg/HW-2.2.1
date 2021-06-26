ef5918ccf9933f21d97f405499c55d73017e8bc2
Denis <denmagg.work@gmail.com>
2021-06-26 06:00:20 +0300

    Initial Commit

---
+++ HW 2.2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2.2.1
+//
+//  Created by Denis Medvedev on 26/06/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
